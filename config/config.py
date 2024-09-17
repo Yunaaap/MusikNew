e53c50a5222e6d25e40c0df1adf792be04ebff02
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-17 23:58:40 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -82,7 +82,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "",
+    "https://github.com/Vooyage21/MusicPlayer",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
