95c1c3c09c2a44cfa309fe78e4b0a09d6f8a71a2
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-18 16:33:51 +0700

    Update ping.py

--- YukkiMusic/plugins/tools/ping.py
+++ YukkiMusic/plugins/tools/ping.py
@@ -23,9 +23,7 @@ from YukkiMusic.utils.inline import support_group_markup
 @app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
-    response = await message.reply_photo(
-        photo=PING_IMG_URL,
-        caption=_["ping_1"].format(app.mention),
+    response = await message.reply_text(_["ping_1"].format(app.mention),
     )
     start = datetime.now()
     pytgping = await Yukki.ping()
