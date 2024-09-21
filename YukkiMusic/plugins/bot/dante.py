0f44a0d6c74c0e2a20d028f696d3ecef3a54e94d
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-21 22:44:58 +0700

    Update dante.py

--- YukkiMusic/plugins/bot/dante.py
+++ YukkiMusic/plugins/bot/dante.py
@@ -5,7 +5,7 @@ from YukkiMusic import app
 
 #--------------------------
 
-MUST_JOIN = "ForwardRepeat"
+MUST_JOIN = "NevcTech"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
