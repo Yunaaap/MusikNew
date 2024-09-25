b2222fd0fe5aadf485cabfb6586196b0f6b3717a
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-26 04:02:04 +0700

    Update dir.py

--- YukkiMusic/core/dir.py
+++ YukkiMusic/core/dir.py
@@ -1,18 +1,22 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the "MIT License".
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 import logging
 import os
 import sys
 from os import listdir, mkdir
 
 from config import TEMP_DB_FOLDER
+
+# remove all files on startup  that contains these extentions
+files = [
+    ".jpg",
+    ".jpeg",
+    ".mp3",
+    ".m4a",
+    ".mp4",
+    ".webm",
+    ".png",
+    ".session",
+    ".session-journal",
+]
 
 
 def dirr():
@@ -27,14 +31,7 @@ def dirr():
         sys.exit()
 
     for file in os.listdir():
-        if (
-            file.endswith(".jpg")
-            or file.endswith(".jpeg")
-            or file.endswith(".mp3")
-            or file.endswith(".png")
-            or file.endswith(".session")
-            or file.endswith(".session-journal")
-        ):
+        if any(file.endswith(ext) for ext in files):
             os.remove(file)
 
     if downloads_folder not in listdir():
