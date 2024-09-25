0a3013e19f378b4c75e8e11ebfd5b83096102d64
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-26 03:54:19 +0700

    Update logging.py

--- YukkiMusic/logging.py
+++ YukkiMusic/logging.py
@@ -3,6 +3,12 @@ from logging.handlers import RotatingFileHandler
 
 from config import LOG_FILE_NAME
 
+
+class SuppressBSONFilter(logging.Filter):
+    def filter(self, record):
+        return "bson.errors.InvalidDocument" not in record.getMessage()
+
+
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
@@ -13,11 +19,18 @@ logging.basicConfig(
     ],
 )
 
+logger = logging.getLogger()
+logger.addFilter(SuppressBSONFilter())
+
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 logging.getLogger("pymongo").setLevel(logging.ERROR)
-logging.getLogger("ntgcalls").setLevel(logging.ERROR)
 logging.getLogger("httpx").setLevel(logging.ERROR)
+
+# Setting ntgcalls logger level and disabling propagation
+ntgcalls_logger = logging.getLogger("ntgcalls")
+ntgcalls_logger.setLevel(logging.CRITICAL)
+ntgcalls_logger.propagate = False
 
 
 def LOGGER(name: str) -> logging.Logger:
