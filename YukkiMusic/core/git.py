e2600b9cd6829186b79323d9debd450ea37da21d
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-28 20:26:31 +0700

    Update git.py

--- YukkiMusic/core/git.py
+++ YukkiMusic/core/git.py
@@ -80,6 +80,6 @@ def git():
     try:
         nrs.pull(config.UPSTREAM_BRANCH)
     except GitCommandError:
-            repo.git.reset("--hard", "FETCH_HEAD")
+        repo.git.reset("--hard", "FETCH_HEAD")
         install_req("pip3 install --no-cache-dir -r requirements.txt")
         LOGGER(__name__).info(f"Fetching updates from upstream repository...")
